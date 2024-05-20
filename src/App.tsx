06825134bb67f875b7a17151b11983def4320d02
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-20 16:50:58 -0300

    feat: add Home Page and React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Header from './components/header/header.component'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//Pages
+import HomePage from './pages/home/home.page'
 
 const App = () => {
-  return <Header />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,12 @@
+// components
+import Header from '../../components/header/header.component'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default HomePage
