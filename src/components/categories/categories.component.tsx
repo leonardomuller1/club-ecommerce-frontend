f0769d42860da4554a3a307d43ebc114e21d0647
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-20 17:31:20 -0300

    feat: add Categories Component

---
+++ src/components/categories/categories.component.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+//utilities
+import Category from '../../types/category.types'
+import env from '../../config/env.config'
+
+//styles
+import './categories.styles.css'
+
+const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([])
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+
+      setCategories(data)
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+
+  console.log({ categories })
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  return (
+    <div className='categories-container'>
+      <div className='categories-content'></div>
+    </div>
+  )
+}
+
+export default Categories
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,10 +1,12 @@
 // components
+import Categories from '../../components/categories/categories.component'
 import Header from '../../components/header/header.component'
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Categories />
     </div>
   )
 }
