42aad990e2bac0291778e8a03fa4360e00e02982
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-21 14:22:31 -0300

    feat: add script that populates firestore db

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+const { getFirestore } = require('firebase/firestore')
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDMGHez5hfHsojIwRseBFHEqyOYPfFer00',
