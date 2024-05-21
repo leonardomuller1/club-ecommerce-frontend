c9e1d0b48a9ab8c716d23ed4f6b3ad92f649bc7f
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-21 15:24:47 -0300

    fix: Category Item Component background image

--- src/components/category-item/category-item.styles.ts
+++ src/components/category-item/category-item.styles.ts
@@ -19,7 +19,12 @@ export const CategoryItemContainer = styled.div<CategoryItemContainerProps>`
   box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
   background-color: rgba(0, 0, 0, 0.3);
   background-blend-mode: color;
-  background-color: ${(props) => `url('${props.backgroundImage}')`};
+  background-image: ${(props) => `url('${props.backgroundImage}')`};
+
+  @media (max-width: 768px) {
+    padding: 0 50px 0 50px;
+    height: 300px;
+  }
 `
 
 export const CategoryName = styled.div`
