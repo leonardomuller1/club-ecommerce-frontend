7578b2171efe9c13dfe9feddb28fd33f0227e03a
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-24 19:38:11 -0300

    feat: handle multiple products on cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -32,8 +32,22 @@ const CartContextProvider: FunctionComponent<CartContextProviderProps> = ({
     setIsVisible((prevState) => !prevState)
   }
 
-  const addProductToCart = (procuct: Product) => {
-    setProducts((prevState) => [...prevState, { ...procuct, quantity: 1 }])
+  const addProductToCart = (product: Product) => {
+    const productIsAlreadyInCart = products.some(
+      (item) => item.id === product.id
+    )
+
+    if (productIsAlreadyInCart) {
+      return setProducts((products) => [
+        ...products.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      ])
+    }
+
+    setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
   }
 
   return (
